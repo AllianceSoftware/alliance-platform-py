ea860a1a7e00836d0aaca261ee84c988decaf776
David Coates <david.coates@alliancesoftware.com.au>
2024-05-14 12:08:46 +1000

    fix: sphinx doc setting
    Remove `extensions` setting from individual projects, only set it in
    the root for multiproject compat
    
    Remove invalid `html_static_path` setting (directory doesn't exist)

--- docs/conf.py
+++ docs/conf.py
@@ -60,4 +60,3 @@ intersphinx_disabled_reftypes = ["*"]
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_rtd_theme"
-html_static_path = ["_static"]
--- packages/ap-frontend/docs/conf.py
+++ packages/ap-frontend/docs/conf.py
@@ -13,8 +13,6 @@ author = "Alliance Software"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon", "sphinx_rtd_theme"]
-
 templates_path = ["_templates"]
 exclude_patterns: list[str] = []
 
@@ -24,4 +22,3 @@ autodoc_typehints = "description"
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_rtd_theme"
-html_static_path = ["_static"]
