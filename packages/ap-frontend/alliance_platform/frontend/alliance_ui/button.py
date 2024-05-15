d9cac4845a893f618e0077283ce16efcde8b4156
Micah Klazinga <micah.klazinga@alliancesoftware.com.au>
2024-05-15 11:50:58 +1000

    fix: update number of backticks for inline markup

--- packages/ap-frontend/alliance_platform/frontend/alliance_ui/button.py
+++ packages/ap-frontend/alliance_platform/frontend/alliance_ui/button.py
@@ -7,7 +7,7 @@ from .utils import get_module_import_source
 
 def button(parser: template.base.Parser, token: template.base.Token):
     """
-    Render a `Button` component from the Alliance UI React library with the specified props.
+    Render a ``Button`` component from the Alliance UI React library with the specified props.
 
     If used for a link, the :function:`~alliance_platform.templatetags.alliance_ui.url_with_perms_filter`
     or :function:`~alliance_platform.frontend.templatetags.alliance_ui.url_filter` can be used to handle
@@ -21,7 +21,7 @@ def button(parser: template.base.Parser, token: template.base.Token):
 
 
 def button_group(parser: template.base.Parser, token: template.base.Token):
-    """Render a `ButtonGroup` component from the Alliance UI React library with the specified props."""
+    """Render a ``ButtonGroup`` component from the Alliance UI React library with the specified props."""
     return parse_component_tag(
         parser,
         token,
