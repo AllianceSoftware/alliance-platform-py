a206eff2dabc370972e22fd85c3d60a5647a5a36
Micah Klazinga <micah.klazinga@alliancesoftware.com.au>
2024-11-13 20:34:28 +1100

    feature: rewrite tests

--- packages/ap-audit/test_alliance_platform_audit/models.py
+++ packages/ap-audit/test_alliance_platform_audit/models.py
@@ -1,7 +1,12 @@
-from common_audit import with_audit_model
-from common_audit.registry import AuditRegistry
+from alliance_platform.audit import create_audit_event
+from alliance_platform.audit import with_audit_model
+from alliance_platform.audit.registry import AuditRegistry
+from django.contrib.auth import user_logged_in
+from django.contrib.auth import user_logged_out
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.db.models import CASCADE
+import pghistory
 
 test_audit_registry = AuditRegistry()
 
@@ -11,7 +16,7 @@ class PaymentMethod(models.Model):
     name = models.CharField(max_length=255, blank=True)
 
     class Meta:
-        db_table = "test_common_audit_payment_method"
+        db_table = "test_alliance_platform_audit_payment_method"
 
     def __str__(self):
         return f"{self.name}"
@@ -25,7 +30,7 @@ class Plaza(models.Model):
     name = models.CharField(max_length=255, blank=True)
 
     class Meta:
-        db_table = "test_common_audit_plaza"
+        db_table = "test_alliance_platform_audit_plaza"
 
     def __str__(self):
         return self.name
@@ -46,7 +51,7 @@ class Shop(models.Model):
     payment_methods_accepted = models.ManyToManyField(PaymentMethod, related_name="shops", blank=True)
 
     class Meta:
-        db_table = "test_common_audit_shop"
+        db_table = "test_alliance_platform_audit_shop"
 
     def __str__(self):
         return f"{self.plaza} - {self.name} ({dict(ShopCategory.choices)[self.category]})"
@@ -61,7 +66,7 @@ class Profile(models.Model):
     name = models.CharField(max_length=100)
 
     class Meta:
-        db_table = "test_common_audit_profile"
+        db_table = "test_alliance_platform_audit_profile"
 
 
 @with_audit_model(
@@ -73,7 +78,7 @@ class MemberProfile(Profile):
     member_id = models.IntegerField()
 
     class Meta:
-        db_table = "test_common_audit_member_profile"
+        db_table = "test_alliance_platform_audit_member_profile"
 
 
 @with_audit_model(
@@ -85,7 +90,7 @@ class SuperMemberProfile(MemberProfile):
     award_points = models.IntegerField()
 
     class Meta:
-        db_table = "test_common_audit_super_member_profile"
+        db_table = "test_alliance_platform_audit_super_member_profile"
 
 
 @with_audit_model(
@@ -96,4 +101,37 @@ class AuthorProfile(Profile):
     homepage = models.URLField()
 
     class Meta:
-        db_table = "test_common_audit_author_profile"
+        db_table = "test_alliance_platform_audit_author_profile"
+
+
+@with_audit_model(
+    registry=test_audit_registry,
+    related_name="user_auditevent",
+    manual_events=["LOGIN"],
+)
+class User(AbstractUser):
+    user_type = "user"
+
+    class Meta:
+        db_table = "test_alliance_platform_audit_custom_user"
+
+
+def track_login(sender, user, **kwargs):
+    # Always use the base user
+    if hasattr(user, "user_ptr"):
+        user = user.user_ptr
+    create_audit_event(user, "LOGIN")
+
+
+def track_logout(sender, user, **kwargs):
+    if user:
+        # logout is a GET so wont trigger middleware; manually add context here.
+        with pghistory.context(user=user.pk):
+            # Always use the base user
+            if hasattr(user, "user_ptr"):
+                user = user.user_ptr
+            create_audit_event(user, "LOGOUT")
+
+
+user_logged_in.connect(track_login)
+user_logged_out.connect(track_logout)
