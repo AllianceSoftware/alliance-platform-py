439c8749a4a95598759244166a34e8d2e6583b31
Micah Klazinga <micahklaz@hotmail.com>
2024-12-28 17:03:46 +1100

    fix: add testing for UI package

--- packages/ap-core/alliance_platform/base_settings.py
+++ packages/ap-core/alliance_platform/base_settings.py
@@ -21,7 +21,7 @@ def maybe_import_string(val: Any | None):
     return val
 
 
-SettingKey = Literal["CORE", "FRONTEND", "CODEGEN", "STORAGE", "AUDIT"]
+SettingKey = Literal["CORE", "FRONTEND", "CODEGEN", "STORAGE", "AUDIT", "UI"]
 
 
 class AlliancePlatformSettingsBase:
