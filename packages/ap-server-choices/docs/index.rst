d40e8416ee693247de13442934fcae0588db1afb
Micah Klazinga <micahklaz@hotmail.com>
2025-02-03 11:17:09 +1100

    feature: initial server choices package setup

---
+++ packages/ap-server-choices/docs/index.rst
@@ -0,0 +1,17 @@
+===========================================================
+Alliance Platform Server choices
+===========================================================
+
+TODO
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   installation
+   settings
+   legacy-migration
+   usage
+   api
+
+.. include:: ../../ap-core/docs/_sidebar.rst.inc
--- scripts/build-docs.sh
+++ scripts/build-docs.sh
@@ -22,8 +22,10 @@ PROJECT=ui sphinx-autobuild --port=56680 -a --watch packages/ap-ui/ docs _docs-b
 pid_ui=$!
 PROJECT=pdf sphinx-autobuild --port=56681 -a --watch packages/ap-pdf/ docs _docs-build/pdf &
 pid_pdf=$!
+PROJECT=server-choices sphinx-autobuild --port=56682 -a --watch packages/ap-server-choices/ docs _docs-build/server-choices &
+pid_server_choices=$!
 sphinx-autobuild --port=56675 --open-browser -a --watch packages/ap-core/ docs _docs-build/core &
 pid_core=$!
 
 # Wait for all background processes to finish
-wait $pid_core $pid_frontend $pid_codegen $pid_storage $pid_audit $pid_ui $pid_pdf
+wait $pid_core $pid_frontend $pid_codegen $pid_storage $pid_audit $pid_ui $pid_pdf $pid_server_choices
