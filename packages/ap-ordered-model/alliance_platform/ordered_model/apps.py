72fb2201281da7485bc169fd1e3360a28ad6f151
Micah Klazinga <micahklaz@hotmail.com>
2025-04-02 13:40:09 +1100

    fix: remove unneeded dependencies and settings

--- packages/ap-ordered-model/alliance_platform/ordered_model/apps.py
+++ packages/ap-ordered-model/alliance_platform/ordered_model/apps.py
@@ -1,4 +1,3 @@
-from alliance_platform.ordered_model.settings import ap_ordered_model_settings
 from django.apps.config import AppConfig
 
 
@@ -6,6 +5,3 @@ class AlliancePlatformOrderedModelConfig(AppConfig):
     name = "alliance_platform.ordered_model"
     verbose_name = "Alliance Platform Ordered Model"
     label = "alliance_platform_ordered_model"
-
-    def ready(self):
-        ap_ordered_model_settings.check_settings()
