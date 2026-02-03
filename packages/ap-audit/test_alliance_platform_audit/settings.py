181d269d004893fbe4dd4a781635707cce978dfa
David Coates <david.coates@alliancesoftware.com.au>
2026-02-03 12:21:32 +1100

    fix: remove tox, test running enhancements, readme updates

--- packages/ap-audit/test_alliance_platform_audit/settings.py
+++ packages/ap-audit/test_alliance_platform_audit/settings.py
@@ -74,6 +74,7 @@ INSTALLED_APPS = (
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
+    "django.contrib.postgres",
     "pghistory",
     "pgtrigger",
     "hijack",
